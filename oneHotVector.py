0e62b1f66dccbbe8564f5d26c2bfee41bb5eeeab
DeanHapoel27 <deangg27@gmail.com>
2025-04-24 16:01:26 +0300

    Create oneHotVector.py

---
+++ oneHotVector.py
@@ -0,0 +1,20 @@
+from sklearn.preprocessing import OneHotEncoder
+from sklearn.compose import ColumnTransformer
+from sklearn.preprocessing import StandardScaler
+
+categorical_features = ['class', 'gender', 'AgeCategory'] 
+ # שמות המשתנים הקטגוריים
+numeric_features = ['height_cm','weight_kg','body fat_%','diastolic',
+ 'systolic','gripForce','sit and bend forward_cm','sit-ups counts','broad jump_cm',]  # שמות הנומריים
+
+preprocessor = ColumnTransformer(
+    transformers=[
+        ('cat', OneHotEncoder(), categorical_features)
+    ],
+    remainder='passthrough'
+)
+
+X_encoded = preprocessor.fit_transform(new_df)
+
+scaler = StandardScaler()
+X_scaled = scaler.fit_transform(X_encoded)
