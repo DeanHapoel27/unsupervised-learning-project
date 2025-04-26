3c1b00b1f29b8d3932e85975d202125f6e08e863
DeanHapoel27 <deangg27@gmail.com>
2025-04-26 15:01:17 +0300

    Update Anomalies_detection.py

--- Anomalies_detection.py
+++ Anomalies_detection.py
@@ -1,11 +1,21 @@
 import numpy as np
+import pandas as pd
+import seaborn as sns
 import matplotlib.pyplot as plt
+from scipy import stats
+from scipy.spatial import ConvexHull
+
+from sklearn.preprocessing import StandardScaler, OneHotEncoder
+from sklearn.compose import ColumnTransformer
 from sklearn.decomposition import PCA
-from sklearn.cluster import KMeans
-from sklearn.mixture import GaussianMixture
+from sklearn.cluster import KMeans, AgglomerativeClustering, DBSCAN, MeanShift, Birch
+from sklearn.metrics import silhouette_score, mutual_info_score
 from sklearn.ensemble import IsolationForest
 from sklearn.svm import OneClassSVM
-from sklearn.metrics import mutual_info_score
+from sklearn.mixture import GaussianMixture
+from sklearn.neighbors import NearestNeighbors, kneighbors_graph
+import igraph
+import leidenalg
 
 
 def run_anomalies_detection():
