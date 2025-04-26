7802c3373604606b2a7b00d870ba9f871c1e2cf2
DeanHapoel27 <deangg27@gmail.com>
2025-04-26 15:01:55 +0300

    Update Mutual_Info_algos_ext_variables.py

--- Mutual_Info_algos_ext_variables.py
+++ Mutual_Info_algos_ext_variables.py
@@ -1,12 +1,21 @@
-\\best amount of clusters acorrding to every algorithm
-
-import pandas as pd
 import numpy as np
-from sklearn.cluster import KMeans, AgglomerativeClustering
-from sklearn.mixture import GaussianMixture
-from sklearn.metrics import mutual_info_score
-from sklearn.decomposition import PCA
+import pandas as pd
+import seaborn as sns
 import matplotlib.pyplot as plt
+from scipy import stats
+from scipy.spatial import ConvexHull
+
+from sklearn.preprocessing import StandardScaler, OneHotEncoder
+from sklearn.compose import ColumnTransformer
+from sklearn.decomposition import PCA
+from sklearn.cluster import KMeans, AgglomerativeClustering, DBSCAN, MeanShift, Birch
+from sklearn.metrics import silhouette_score, mutual_info_score
+from sklearn.ensemble import IsolationForest
+from sklearn.svm import OneClassSVM
+from sklearn.mixture import GaussianMixture
+from sklearn.neighbors import NearestNeighbors, kneighbors_graph
+import igraph
+import leidenalg
 
 def run_mutual_info_algos_ext_variables():
 
