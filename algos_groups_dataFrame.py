cb38de9133333c5a014b04d80815bdd71a26e6a3
DeanHapoel27 <deangg27@gmail.com>
2025-04-26 15:02:15 +0300

    Update algos_groups_dataFrame.py

--- algos_groups_dataFrame.py
+++ algos_groups_dataFrame.py
@@ -1,7 +1,22 @@
 import numpy as np
-import matplotlib.pyplot as plt
 import pandas as pd
+import seaborn as sns
+import matplotlib.pyplot as plt
 from scipy import stats
+from scipy.spatial import ConvexHull
+
+from sklearn.preprocessing import StandardScaler, OneHotEncoder
+from sklearn.compose import ColumnTransformer
+from sklearn.decomposition import PCA
+from sklearn.cluster import KMeans, AgglomerativeClustering, DBSCAN, MeanShift, Birch
+from sklearn.metrics import silhouette_score, mutual_info_score
+from sklearn.ensemble import IsolationForest
+from sklearn.svm import OneClassSVM
+from sklearn.mixture import GaussianMixture
+from sklearn.neighbors import NearestNeighbors, kneighbors_graph
+import igraph
+import leidenalg
+
 
 def run_algos_groups_dataframe():
     #take 25 different and equal groups from data base
