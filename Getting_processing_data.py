7a36cd7dc803939345cba43091a6b74f92743f88
DeanHapoel27 <deangg27@gmail.com>
2025-04-26 15:01:36 +0300

    Update Getting_processing_data.py

--- Getting_processing_data.py
+++ Getting_processing_data.py
@@ -1,7 +1,22 @@
+import numpy as np
 import pandas as pd
-from sklearn.preprocessing import OneHotEncoder
+import seaborn as sns
+import matplotlib.pyplot as plt
+from scipy import stats
+from scipy.spatial import ConvexHull
+
+from sklearn.preprocessing import StandardScaler, OneHotEncoder
 from sklearn.compose import ColumnTransformer
-from sklearn.preprocessing import StandardScaler
+from sklearn.decomposition import PCA
+from sklearn.cluster import KMeans, AgglomerativeClustering, DBSCAN, MeanShift, Birch
+from sklearn.metrics import silhouette_score, mutual_info_score
+from sklearn.ensemble import IsolationForest
+from sklearn.svm import OneClassSVM
+from sklearn.mixture import GaussianMixture
+from sklearn.neighbors import NearestNeighbors, kneighbors_graph
+import igraph
+import leidenalg
+
 
 def run_getting_processing_data():
   df = pd.read_csv('bodyPerformance.csv')
